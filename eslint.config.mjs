a01ec94534693baacc8b5cdcf821c0de9b7e7129
Sharon <sbarrienos@utp.edu.pe>
2025-10-21 22:12:25 -0500

    chore: clase1 - estructura base del proyecto

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    ignores:[
+      "node_modules/**",
+      "dist/**",
+      "reports/**",
+      "artifacts/**",
+      "eslint.config.mjs"
+    ]
+  },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+
+    {
+      files: ["**/*.ts"],
+      languageOptions: {
+        sourType: "module",
+        globals: { ...globals.browser, ...globals.node }
+      },
+      rules: {
+        "no-console": "off"
+      }
+  },
+  eslintConfigPrettier
+]);
